495874651aba77c8ab60bb6cb6d373029cf83232
Alejandro <180036@iessierradeguara.com>
2022-12-09 09:47:41 +0100

    segundo commit apuntando a master

--- ServerHora/src/server/DataTextServer.java
+++ ServerHora/src/server/DataTextServer.java
@@ -6,7 +6,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
 
-public class DataTextServer {
+public class DataTextServer{
 
 	public static final int PORT = 3001;
 	public static void main(String[] args) {
