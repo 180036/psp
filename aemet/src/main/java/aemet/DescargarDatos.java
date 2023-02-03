8bae5f56920e327ca9721c1fbc2dc80adb52d218
Alejandro <180036@iessierradeguara.com>
2023-02-03 09:32:34 +0100

    Commit clase EstacionMeteo imperfecciones y creacion objeto Geo850

--- aemet/src/main/java/aemet/DescargarDatos.java
+++ aemet/src/main/java/aemet/DescargarDatos.java
@@ -5,6 +5,10 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class DescargarDatos {
 
@@ -23,8 +27,20 @@ public class DescargarDatos {
 		System.out.println(res.body());
 		System.out.println(res);
 		
+		//https://opendata.aemet.es/opendata/sh/2f2056ba
 		//Usar jackson para obtener la cadena de datos y de metadatos
+		ObjectMapper mapper = new ObjectMapper();
+		String urlDatos = mapper.readTree(res.body()).at("/datos").asText(); //accede al contenido de la propiedad del objeto json "datos"
+		
 		//hacer las peticiones correspondientes de esos dos JSON
+		HttpRequest reqDatos = HttpRequest.newBuilder().uri(URI.create(urlDatos)).GET().build();
+		HttpResponse<String> resDatos = cliente.send(reqDatos, HttpResponse.BodyHandlers.ofString());
+		
+		//Mapeamos los datos en una lista.
+		List<EstacionMeteo> listaEstaciones = mapper.readValue(resDatos.body(), new TypeReference<List<EstacionMeteo>>() {});
+		
+		System.out.println(listaEstaciones.get(0).getUbi());
+		System.out.println(listaEstaciones.get(0).getTa());
 		//coger el valor deseado filtrado por el idioma de Huesca(9901X)
 		
 	}
